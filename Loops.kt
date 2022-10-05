39814b99a8c2e3b898f554e8befc9ba12122cad1
Otiego <ronald.otiego@gmail.com>
2022-10-05 16:57:09 +0300

    Added loops

--- Loops.kt
+++ Loops.kt
@@ -2,7 +2,19 @@ fun main(args: Array<String>) {
     val rebels = arrayListOf("Madlobo", "Leiah", "Niklause", "Han Solo", "Chris")
     val rebelVehiclesMap = hashMapOf( "Solo" to "Millenium", "Luke" to "Landspeeder", "Boba Fette" to "Rocket Pack")
 
+
+    val counter =0
     for (rebel in rebels){
-        println(rebel)
+        println("Name: $rebel")
+//        rebels.indexOf("ui")
+    }
+    for ((key, value) in rebelVehiclesMap){
+        println("$key gets around in their $value")
+    }
+
+    var x =0
+    while (x<=10){
+        println(x)
+        x++
     }
 }
