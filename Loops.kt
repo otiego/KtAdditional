79633c4a648cf44e0ddfa37ad8a69ad7e467e00b
Otiego <ronald.otiego@gmail.com>
2022-10-05 16:08:08 +0300

    added Loops.kt

---
+++ Loops.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    val rebels = arrayListOf("Madlobo", "Leiah", "Niklause", "Han Solo", "Chris")
+    val rebelVehiclesMap = hashMapOf( "Solo" to "Millenium", "Luke" to "Landspeeder", "Boba Fette" to "Rocket Pack")
+
+    for (rebel in rebels){
+        println(rebel)
+    }
+}
